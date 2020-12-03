89b547b83139eed6098bf66fbc95501f6c7418c2
Rayhaan Khalid <rkhalid@me.com>
2020-12-03 13:19:55 +0000

    fix 02:00 not converting to 24hr bug

--- src/index.js
+++ src/index.js
@@ -206,15 +206,20 @@ function checkZero(num) {
 
 function convertTo24Hr(time) {
   //check if hour is 12 or 11 if so do not add 12
-  if (time[1] === "2" || time.substring(0, 2) === "11") return time;
+  console.log(time)
+
+  if (time.substring(0,2) === "12" || time.substring(0, 2) === "11") return time;
+  console.log(time)
 
   if (time[0] !== "0") {
     time = "0" + time;
   }
   let hours = time.substring(0, 2);
+  console.log(time)
 
   hours = parseInt(hours) + 12;
   return hours + time.substring(2);
+  
 }
 
 function setDate() {
